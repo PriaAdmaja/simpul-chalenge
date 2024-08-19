8044f069f9b50409c487d29b05f0278ac945ea5c
Pria Admaja <pria.admaja@havedev.com>
2024-08-20 00:34:27 +0700

    fixing chat group

--- src/component/chat/chat-group/index.tsx
+++ src/component/chat/chat-group/index.tsx
@@ -12,16 +12,17 @@ const ChatGroup = ({
   name: string;
   description: string;
 }) => {
+  const slicedDescription = description.length > 100 ? `${description.slice(0, 93)} ...` : description;
   return (
     <section className="flex gap-4">
       <IconChatGroup />
       <section className="flex-1">
-        <div className="flex ">
+        <div className="flex mb-1 gap-2">
           <p className="flex-1 text-base font-bold text-primary cursor-pointer">{title}</p>
           <p>{dayjs(date).format("DD/MM/YYYY hh:mm")}</p>
         </div>
         <p className="text-sm font-bold">{name} :</p>
-        <p className="text-sm">{description}</p>
+        <p className="text-sm">{slicedDescription}</p>
       </section>
     </section>
   );
